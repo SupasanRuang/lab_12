fb4d4aa07fc6083f4c386c83a7dc3e180631819b
SupasanRuang <supasan_ruang@cmu.ac.th>
2021-12-21 17:08:13 +0700

    lab12_1.cpp completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,74 @@
+#include<iostream>
+#include<ctime>
+#include<cstdlib>
+using namespace std ;
+
+
+int main()
+{
+    srand(time(0));
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    cout << "You will get ";
+    int x=rand()%9;
+    if(x==0)
+    {
+        cout << "A";
+    }
+    else if(x==1)
+    {
+        cout << "B+";
+    }
+    else if(x==2)
+    {
+        cout << "B";
+    }
+    else if(x==3)
+    {
+        cout << "C+";
+    }
+    else if(x==4)
+    {
+        cout << "C";
+    }
+    else if(x==5)
+    {
+        cout << "D+";
+    }
+    else if(x==6)
+    {
+        cout << "D";
+    }
+    else if(x==7)
+    {
+        cout << "F";
+    }
+    else if(x==8)
+    {
+        cout << "W";
+    }
+    cout << " in this 261102.";
+   
+
+return 0;
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
 // cout << "Press Enter 3 times to reveal your future.";
 // cout << "You will get A in this 261102.";
